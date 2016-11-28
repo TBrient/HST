d072b5d5d6bc9080de06db652774950304926970
TBrient <superheroguy89@gmail.com>
2016-11-28 06:34:00 -0500

    began work on graphs

--- app/src/main/java/workoutapp/tyler/workoutapplication/calculationFragment.java
+++ app/src/main/java/workoutapp/tyler/workoutapplication/calculationFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
@@ -63,8 +65,9 @@ public class calculationFragment extends Fragment  {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculation, container, false);
+        //Creates a view
+        View view = inflater.inflate(R.layout.fragment_calculation , container, false);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
